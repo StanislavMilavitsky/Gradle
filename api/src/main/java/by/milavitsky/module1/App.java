d3e4ff10ba9d9e15c45e5b74aca81da471cd4562
Stanislav Milavitsky <stanislav.milavitsky@gmail.com>
2022-01-27 15:00:38 +0300

    add part two gradle

---
+++ api/src/main/java/by/milavitsky/module1/App.java
@@ -0,0 +1,10 @@
+package by.milavitsky.module1;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        boolean condition = Utils.isAllPositiveNumbers("12", "79");
+        System.out.println(condition);
+    }
+}
