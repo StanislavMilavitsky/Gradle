ca0cf84a65b58918d7ba31d341d58f51d5ee1e04
Stanislav Milavitsky <stanislav.milavitsky@gmail.com>
2022-01-27 10:31:53 +0300

    add gradle on github

---
+++ src/main/java/by/milavitsky/module1/StringUtils.java
@@ -0,0 +1,16 @@
+package by.milavitsky.module1;
+
+import static org.apache.commons.lang3.math.NumberUtils.INTEGER_ZERO;
+import static org.apache.commons.lang3.math.NumberUtils.compare;
+import static org.apache.commons.lang3.math.NumberUtils.createInteger;
+import static org.apache.commons.lang3.math.NumberUtils.isCreatable;
+
+public class StringUtils {
+    public static boolean isPositiveNumber(String str) {
+        if (isCreatable(str)) {
+            Integer number = createInteger(str);
+            return compare(number, INTEGER_ZERO) > 0;
+        }
+        return false;
+    }
+}
